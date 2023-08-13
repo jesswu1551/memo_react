b82e364203a7328d14ed845fa557470989be1a49
jess.wu <jesswu1551@gmail.com>
2023-08-13 16:46:17 +0800

    update deploy config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/react_course/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/memo_react/' : '/',
   plugins: [react()],
 });
